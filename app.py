1b2cdb7ba8f371e85500d362ca5ef05f5bc795e3
Jairo Serrano <jairo.serrano@gmail.com>
2025-03-14 19:53:27 -0500

    guardado correcto del formulario de contacto y datos.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
@@ -8,7 +8,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 @app.route("/galletas")
 def galletas():
@@ -20,13 +19,25 @@ def galleta(galleta):
 
 @app.route("/contacto", methods=["GET"])
 def contacto():
-    return render_template("contacto.html")
+    mensaje = request.args.get("mensaje")
+    return render_template("contacto.html", mensaje=mensaje)
 
+"""
+Recepción de contactos para venta de productos.
+"""
 @app.route("/contacto", methods=["POST"])
 def guardar_contacto():
-    #return render_template("contacto.html")
-    return request.form
+    nombre = request.form.get("nombre")
+    apellidos = request.form.get("apellidos")
+    email = request.form.get("email")
+    telefono = request.form.get("telefono")
+    ciudad = request.form.get("ciudad")
+    # Guardar en archivo de texto plano con formato csv, separado por comas.
+    with open("database/contactos.txt", "a") as file:
+        file.write(f"{nombre},{apellidos},{email},{telefono},{ciudad}\n")
 
+    # Redireccionar a la página de contacto.
+    return redirect(url_for("contacto", mensaje="Contacto guardado."))
 
 if __name__ == "__main__":
     app.run(debug=True)
